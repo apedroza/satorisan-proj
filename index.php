600351d28a59dacf5d384d0cf21e7950a928e3d3
Adam Pedroza <apedroza6@cnm.edu>
2017-01-12 08:52:28 -0700

    Added title and beginning of persona

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+
+		<title>DATA DESIGN PROJ</title>
+
+	</head>
+	<body>
+		<header>
+			<h1 class="foo-heading">Persona Satorisan Data Design</h1>
+		</header>
+		<main>
+			<p id="description">Name: Frank Deer
+			Age:28
+			</p>
+		</main>
+	</body>
+</html>
